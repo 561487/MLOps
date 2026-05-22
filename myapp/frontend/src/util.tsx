52e5382b2eaa26099861a8c209286fa5c231ed18
root <root@ISS018737000071.isoftstone.com>
2026-05-22 10:53:37 +0800

    MLops图标、logo、标题、水印修改

--- myapp/frontend/src/util.tsx
+++ myapp/frontend/src/util.tsx
@@ -183,7 +183,7 @@ export function drawWater() {
     if (!id)
         id = '';
     const content = username;
-    const src = id + " @Cube Studio";
+    const src = id + " @MLops";
 
     window.onload = function () {
         drawWaterMark({ content: content, src: src });
@@ -198,7 +198,7 @@ export function drawWaterNow() {
     if (!id)
         id = '';
     const content = username;
-    const src = id + " @Cube Studio";
+    const src = id + " @MLops";
 
     drawWaterMark({ content: content, src: src });
 }
