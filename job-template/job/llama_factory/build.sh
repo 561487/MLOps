159a81a6224a13fc8f9af96907c919f9bf5dd9c3
zhangjinmin <zhangjinmin@thtfpc.com>
2026-06-18 09:13:32 +0000

    Revert "更新任务模板配置，新增llama_factory相关目录"
    This reverts commit d70931acbbcca8af9fe222d4ce8dcc56792cdc7a

--- job-template/job/llama_factory/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-REGISTRY=10.121.177.20:8082/mlops
-TAG=20250601
-
-docker build --network=host -t ${REGISTRY}/llama_factory:${TAG} -f job/llama_factory/Dockerfile .
-docker push ${REGISTRY}/llama_factory:${TAG}
