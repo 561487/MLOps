cb4bce0a66b3b7595e0dbee272c178b7275fd580
zhangjinmin <zhangjinmin@isshuangfang.com>
2026-07-06 14:50:32 +0800

    前端构建产物取消以来lock与本地缓存

--- myapp/frontend/config/webpack.config.js
+++ myapp/frontend/config/webpack.config.js
@@ -473,6 +473,9 @@ module.exports = function (webpackEnv) {
                 sourceMaps: shouldUseSourceMap,
                 inputSourceMap: shouldUseSourceMap,
               },
+              resolve: {
+                fullySpecified: false,
+              },
             },
             // "postcss" loader applies autoprefixer to our CSS.
             // "css" loader resolves paths in CSS and adds assets as dependencies.
