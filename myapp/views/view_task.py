c05f1932c880c126e39f1c9a9e13d4682ebfea59
lihongyu <lihongyu@isoftstone.com>
2026-07-22 06:43:23 +0000

    修改地址

--- myapp/views/view_task.py
+++ myapp/views/view_task.py
@@ -631,7 +631,7 @@ class Task_ModelView_Base():
 
             # MLOps 侧变量
             _register_url = conf.get('MLOPS_MONITOR_REGISTER_URL',
-                                     'http://10.121.177.10:18080/training_monitor/api/register')
+                                     'http://10.121.177.20:18080/training_monitor/api/register')
             task_env += "MLOPS_TRAINING_MONITOR_ENABLE=true\n"
             task_env += "MLOPS_TRAINING_MONITOR_TYPE=swanlab\n"
             task_env += f"MLOPS_MONITOR_REGISTER_URL={_register_url}\n"
