8f609253808c586b0c08c2043bdb07d3b26137ed
stu02 <liuguangwei>
2026-07-23 03:18:05 +0000

    feat(msswift): 多节点多卡分布式硬件监控集成 SwanLab
    - 新增 distributed_hardware_monitor.py: 多节点 GPU 采集，Master HTTP 聚合
    - 新增 swift_training_wrapper.py: 训练命令包装器，解析 swift 输出指标
    - 修改 training_monitor.py: BASIC_PREFIXES 新增 hardware/cluster 前缀
    - 修改 launcher.py: K8s Monitor Service + 监控环境变量注入
    - 修改 Dockerfile: 安装 swanlab/psutil，拷贝监控模块
    - 修改 build.sh: 镜像 tag 更新为 20260721-monitor-v3
    - 修改 init-job-template.json: 镜像版本指向监控镜像
    - 修改 view_pipeline/view_task: msswift 默认 SwanLab cloud 模式

--- job-template/job/msswift/build.sh
+++ job-template/job/msswift/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-IMAGE=${IMAGE:-10.121.177.20:8082/mlops/msswift:20260715-v5}
+IMAGE=${IMAGE:-10.121.177.20:8082/mlops/msswift:20260721-monitor-v3}
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/../../.." && pwd)"
 docker build --network=host -t "${IMAGE}" -f "${SCRIPT_DIR}/Dockerfile" "${REPO_ROOT}"
