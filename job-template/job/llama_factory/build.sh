d28915b00a4591069fbc15b224ec25fd6bd6f70b
lihongyu <17436171+lhy919@user.noreply.gitee.com>
2026-08-20 05:22:08 +0000

    chore: 升级 llama-factory 依赖版本并统一镜像 Tag 规范

--- job-template/job/llama_factory/build.sh
+++ job-template/job/llama_factory/build.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
-REGISTRY=10.121.177.20:8082/mlops
-TAG=20250601
+REGISTRY="10.121.177.20:8082"
+PROJECT="mlops"
+IMAGE_NAME="llama_factory"
+IMAGE_VERSION="0.9.5-py313-cu128-r1"
+IMAGE="${REGISTRY}/${PROJECT}/${IMAGE_NAME}:${IMAGE_VERSION}"
 
-docker build --network=host -t ${REGISTRY}/llama_factory:${TAG} -f job/llama_factory/Dockerfile .
-docker push ${REGISTRY}/llama_factory:${TAG}
+docker build --network=host -t "${IMAGE}" -f job/llama_factory/Dockerfile .
+docker push "${IMAGE}"
+
+echo "Published: ${IMAGE}"
