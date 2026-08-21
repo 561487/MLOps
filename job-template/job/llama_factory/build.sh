dd36bf515d892ee92a26c037565528873db6dd34
lihongyu <17436171+lhy919@user.noreply.gitee.com>
2026-08-21 06:12:25 +0000

    llama_factory 镜像修复

--- job-template/job/llama_factory/build.sh
+++ job-template/job/llama_factory/build.sh
@@ -5,10 +5,10 @@ set -ex
 REGISTRY="10.121.177.20:8082"
 PROJECT="mlops"
 IMAGE_NAME="llama_factory"
-IMAGE_VERSION="0.9.5-py313-cu128-r1"
+IMAGE_VERSION="0.9.5-py313-cu128-r4"
 IMAGE="${REGISTRY}/${PROJECT}/${IMAGE_NAME}:${IMAGE_VERSION}"
 
-docker build --network=host -t "${IMAGE}" -f job/llama_factory/Dockerfile .
+docker build --network=host -t "${IMAGE}" -f job/llama_factory/Dockerfile.r4 .
 docker push "${IMAGE}"
 
 echo "Published: ${IMAGE}"
